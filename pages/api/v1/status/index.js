b1d32a07e55ca354898319bce20039920687c5b8
Guilherme Domingues <guilhermedominguesdev@gmail.com>
2025-01-22 23:32:14 +0000

    Adicionando aquivos  e

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-//import database from "../../../../infra/database.js";
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
   response
     .status(200)
     .json({ chave: "Alunos do curso.dev são pessoas a cima da média" });
