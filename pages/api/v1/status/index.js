00d60802f84ef4fc10585931c38cfb712c0458e4
Guilherme Domingues <guilhermedominguesdev@gmail.com>
2025-01-24 00:25:48 +0000

    adicionando campos na API/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas a cima da média" });
+  const updateAt = new Date().toISOString();
+
+  const postgresV = await database.query("SELECT version()");
+  const postgresConexoesAtual = await database.query(
+    "select count(*) from pg_stat_activity",
+  );
+  const postgresConexoesMaxima = await database.query("SHOW max_connections;");
+  console.log(postgresConexoesAtual);
+  response.status(200).json({
+    update_at: updateAt,
+    postgres_version: postgresV.rows,
+    postgres_conection_curently: postgresConexoesAtual.rows,
+    postgres_conection_max: postgresConexoesMaxima.rows,
+  });
 }
 
 export default status;
