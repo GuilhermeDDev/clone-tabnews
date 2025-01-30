b4bc9db226149dccc8e9669003743df329e89741
Guilherme Domingues <guilhermedominguesdev@gmail.com>
2025-01-30 00:08:44 +0000

    A

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -8,12 +8,12 @@ async function status(request, response) {
     "select count(*) from pg_stat_activity",
   );
   const postgresConexoesMaxima = await database.query("SHOW max_connections;");
-  console.log(postgresConexoesAtual);
+
   response.status(200).json({
     update_at: updateAt,
-    postgres_version: postgresV.rows,
-    postgres_conection_curently: postgresConexoesAtual.rows,
-    postgres_conection_max: postgresConexoesMaxima.rows,
+    postgres_version: postgresV.rows[0].version,
+    postgres_conection_curently: postgresConexoesAtual.rows[0].count,
+    postgres_conection_max: postgresConexoesMaxima.rows[0].max_connections,
   });
 }
 
