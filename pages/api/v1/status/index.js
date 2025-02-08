f1ece64c0f3395a5e54db6b48a0ce96447cc048f
Guilherme Domingues <guilhermedominguesdev@gmail.com>
2025-02-08 23:49:20 +0000

    Alteração database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,19 +1,40 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  //data da requisição API
   const updateAt = new Date().toISOString();
-
-  const postgresV = await database.query("SELECT version()");
+  // versão do banco
+  const postgresV = await database.query("SHOW server_version;");
+  const postgresVV = postgresV.rows[0].server_version;
+  //  conections ativas
   const postgresConexoesAtual = await database.query(
-    "select count(*) from pg_stat_activity",
+    "select count(*) from pg_stat_activity;",
   );
+
+  const postgresConectionsC = postgresConexoesAtual.rows[0].count;
+  // max conections
   const postgresConexoesMaxima = await database.query("SHOW max_connections;");
+  const postgresMaxConections = postgresConexoesMaxima.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query(
+    {
+      text: "SELECT * FROM pg_stat_activity where datname = $1;",
+      values: [databaseName],
+    }, //"SELECT * FROM pg_stat_activity where datname = 'salame_db';",
+  );
+  const opened_connections = openedConnections.rows.length;
 
   response.status(200).json({
     update_at: updateAt,
-    postgres_version: postgresV.rows[0].version,
-    postgres_conection_curently: postgresConexoesAtual.rows[0].count,
-    postgres_conection_max: postgresConexoesMaxima.rows[0].max_connections,
+    dependencies: {
+      database: {
+        postgres_version: postgresVV,
+        postgres_conection_curently: parseInt(postgresConectionsC),
+        postgres_conection_max: parseInt(postgresMaxConections),
+        opened_connections: opened_connections,
+      },
+    },
   });
 }
 
--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,14 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
+  //teste de fetch na API
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-
+  // teste de data da requisição
   const responseBody = await response.json();
-
   expect(responseBody.update_at).toBeDefined();
-
   new Date(responseBody.update_at).toISOString();
-
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
-
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
+  // teste de versão do banco
+  expect(responseBody.dependencies.database.postgres_version).toEqual("16.6");
+  // teste de numero de conexões ativas do banco
+  expect(
+    responseBody.dependencies.database.postgres_conection_curently,
+  ).toBeDefined();
+  // teste de numero maximo de conexões do banco
+  expect(responseBody.dependencies.database.postgres_conection_max).toEqual(
+    100,
+  );
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
